0ee29b51b6214adc45444d3ffe3251127a454841
omeszar@live.com <omeszar@live.com>
2013-07-13 20:33:56 -0600

    Before Adding Durandal 2.0

---
+++ Durandal2UserMaint/App_Start/BundleConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace Durandal2UserMaint
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var vendorScriptBundle = new ScriptBundle("~/bundles/vendor")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery-ui-{version}.js")
+                .Include("~/scripts/sammy-{version}.js")
+                .Include("~/scripts/knockout-{version}.js")
+                .Include("~/scripts/q.min.js")
+                .Include("~/scripts/toastr.js")
+                .Include("~/Scripts/jquery.unobtrusive*")
+                .Include("~/Scripts/jquery.validate*")
+                .Include("~/Scripts/bootstrap.js")
+                ;
+
+            vendorScriptBundle.ConcatenationToken = Environment.NewLine;
+
+            bundles.Add(vendorScriptBundle);
+
+            var modernizrScriptBundle = new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*");
+
+            modernizrScriptBundle.ConcatenationToken = Environment.NewLine;
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(modernizrScriptBundle);
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/ie10mobile.css")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/bootstrap-responsive.css")
+                .Include("~/Content/site.css")
+                .Include("~/Content/durandal.css")
+                .Include("~/Content/font-awesome.min.css")
+                .Include("~/Content/toastr.css")
+                );
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
---
+++ Durandal2UserMaint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Durandal2UserMaint.MvcApplication" Language="C#" %>
